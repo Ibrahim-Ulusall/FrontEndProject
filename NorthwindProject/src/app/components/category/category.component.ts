bad676edf46d017b6df9c6841f98858b2a7db372
ibrahim <ibrahimulusal185@gmail.com>
2023-08-30 12:55:24 +0300

    changed files

--- NorthwindProject/src/app/components/category/category.component.ts
+++ NorthwindProject/src/app/components/category/category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/Models/product';
 import { HttpClient } from '@angular/common/http';
 import { ProductResponseModel } from 'src/app/Models/productResponseModel';
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -9,18 +10,8 @@ import { ProductResponseModel } from 'src/app/Models/productResponseModel';
 })
 export class CategoryComponent implements OnInit {
 
-  products: Product[] = [];
-  apiUrl: string = 'https://localhost:7200/api/products/getAll';
-  constructor(private httpClient: HttpClient) { }
-
+  constructor(){}
   ngOnInit(): void {
-  }
-
-  getProducts(): Product[] {
-    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response) => {
-      this.products = response.data;
-    });
-
-    return this.products;
+      
   }
 }
--- NorthwindProject/src/app/components/product/product.component.ts
+++ NorthwindProject/src/app/components/product/product.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from 'src/app/Models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/Models/productResponseModel';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  
-  constructor(){}
+  products: Product[] = [];
+  apiUrl: string = 'https://localhost:7200/api/products/getAll';
+  constructor(private httpClient: HttpClient) { }
+
   ngOnInit(): void {
-    
+  }
+
+  getProducts(): Product[] {
+    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response) => {
+      this.products = response.data;
+    });
+
+    return this.products;
   }
 
 }
