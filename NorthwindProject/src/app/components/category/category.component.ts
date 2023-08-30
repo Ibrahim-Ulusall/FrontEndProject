406da9d5de379af4b7feda7bd31c68441f394c61
ibrahim <ibrahimulusal185@gmail.com>
2023-08-30 12:51:50 +0300

    update

--- NorthwindProject/src/app/components/category/category.component.ts
+++ NorthwindProject/src/app/components/category/category.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/Models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/Models/productResponseModel';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
 
+  products: Product[] = [];
+  apiUrl: string = 'https://localhost:7200/api/products/getAll';
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  getProducts(): Product[] {
+    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response) => {
+      this.products = response.data;
+    });
+
+    return this.products;
+  }
 }
